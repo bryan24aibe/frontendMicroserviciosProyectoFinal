f9a86c32872f5ab43744fe356720e2910feea020
balarai <b24ordonez@gmail.com>
2025-01-30 01:22:15 -0500

    feat: implement CartContext to manage cart state and actions

---
+++ src/context/CartContext.js
@@ -0,0 +1,33 @@
+import { createContext, useState } from "react";
+
+// Crear el contexto del carrito
+export const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  // Función para agregar productos al carrito
+  const addToCart = (product) => {
+    setCart((prevCart) => {
+      const existingProduct = prevCart.find((item) => item.id === product.id);
+      if (existingProduct) {
+        return prevCart.map((item) =>
+          item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+        );
+      } else {
+        return [...prevCart, { ...product, quantity: 1 }];
+      }
+    });
+  };
+
+  // Función para eliminar productos del carrito
+  const removeFromCart = (productId) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== productId));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
