d67d4cebe741befb8ce468c7a5c234b69980a429
balarai <b24ordonez@gmail.com>
2025-01-30 01:24:18 -0500

    feat: add Payment component with payment options information

---
+++ src/pages/Payment.js
@@ -0,0 +1,12 @@
+import "../styles/Payment.css";
+
+function Payment() {
+  return (
+    <div className="payment">
+      <h2>Opciones de Pago</h2>
+      <p>Aceptamos tarjetas de crédito, débito y pagos electrónicos.</p>
+    </div>
+  );
+}
+
+export default Payment;
