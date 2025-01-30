2ff8aa4b334b468783c45e35884af8d3bc9709e0
balarai <b24ordonez@gmail.com>
2025-01-30 01:23:13 -0500

    feat: add Home component with welcome message

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <h2>Bienvenido a nuestra tienda</h2>
+      <p>Encuentra variedad y calidad en repuestos de telefonia.</p>
+      <p>El Telefono de tus sueños.</p>
+    </div>
+  );
+}
+
+export default Home;
