34f3f4a17fd832e696103a7f4e8a151bfa95ba9b
balarai <b24ordonez@gmail.com>
2025-01-30 01:25:43 -0500

    feat: add Promotions component to display special promotions and discounts

---
+++ src/pages/Promotions.js
@@ -0,0 +1,12 @@
+import "../styles/Promotions.css";
+
+function Promotions() {
+  return (
+    <div className="promotions">
+      <h2>Promociones Especiales</h2>
+      <p>Consulta nuestras ofertas y descuentos exclusivos.</p>
+    </div>
+  );
+}
+
+export default Promotions;
