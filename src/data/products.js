702e656677bf4b412f5ca07a1de2db32b133030a
balarai <b24ordonez@gmail.com>
2025-01-30 02:48:18 -0500

    feat: add initial product data for displays

---
+++ src/data/products.js
@@ -0,0 +1,33 @@
+const products = [
+    { 
+      id: 1, 
+      name: "Display Samsung SM-A505G", 
+      price: "$80", 
+      description: "Pantalla original para Samsung Galaxy A50 (SM-A505G)", 
+      image: "https://i.imgur.com/zvCQOZt.jpg"
+    },
+    { 
+      id: 2, 
+      name: "Display Samsung SM-A515F", 
+      price: "$90", 
+      description: "Pantalla original para Samsung Galaxy A51 (SM-A515F)", 
+      image: "https://i.imgur.com/T1uQ37M.jpg"
+    },
+    { 
+      id: 3, 
+      name: "Display Samsung SM-A705F", 
+      price: "$100", 
+      description: "Pantalla original para Samsung Galaxy A70 (SM-A705F)", 
+      image: "https://i.imgur.com/5XhEdwE.jpg"
+    },
+    { 
+      id: 4, 
+      name: "Display Samsung SM-M315F", 
+      price: "$85", 
+      description: "Pantalla original para Samsung Galaxy M31 (SM-M315F)", 
+      image: "https://i.imgur.com/qF7PKrS.jpg"
+    },
+  ];
+  
+  export default products;
+  
