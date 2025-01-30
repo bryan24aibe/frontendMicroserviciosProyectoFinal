fd52451e24e9db2604f3fe6e4a8a64cb21f227e0
balarai <b24ordonez@gmail.com>
2025-01-30 01:26:49 -0500

    feat: add Service component to display technical support information

---
+++ src/pages/Service.js
@@ -0,0 +1,12 @@
+import "../styles/Service.css";
+
+function Service() {
+  return (
+    <div className="service">
+      <h2>Servicio Técnico</h2>
+      <p>Ofrecemos reparación y mantenimiento para tus productos.</p>
+    </div>
+  );
+}
+
+export default Service;
