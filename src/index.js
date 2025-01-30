f7297bc3d9603b027bdfbce017b046a3868de624
balarai <b24ordonez@gmail.com>
2025-01-30 02:47:38 -0500

    chore: update ReactDOM import and use createRoot

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { CartProvider } from './context/CartContext';  // 🔹 Importar el contexto del carrito
+import React from "react";
+import ReactDOM from "react-dom/client"; // ✅ Importa desde "react-dom/client"
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root")); // ✅ Usar createRoot()
 root.render(
   <React.StrictMode>
-    <CartProvider>  {/* 🔹 Envolver la app dentro del contexto del carrito */}
-      <App />
-    </CartProvider>
+    <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
